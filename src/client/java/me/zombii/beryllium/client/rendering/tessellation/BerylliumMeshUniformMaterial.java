122b25241cab32a055d56b8dc4fe7a03329a5a51
NikRasOff <rasschepkinnik@gmail.com>
2026-06-30 18:47:10 +0300

    Moved binding all the shader uniforms into BerylliumMeshUniformMaterial

--- src/client/java/me/zombii/beryllium/client/rendering/tessellation/BerylliumMeshUniformMaterial.java
+++ src/client/java/me/zombii/beryllium/client/rendering/tessellation/BerylliumMeshUniformMaterial.java
@@ -3,6 +3,9 @@ package me.zombii.beryllium.client.rendering.tessellation;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector3;
+import finalforeach.cosmicreach.entities.player.Player;
+import finalforeach.cosmicreach.gamestates.InGame;
+import finalforeach.cosmicreach.settings.GraphicsSettings;
 import finalforeach.cosmicreach.world.Sky;
 import me.zombii.beryllium.client.rendering.opengl.shader.BerylliumShaderProgram;
 import org.lwjgl.opengl.GL20;
@@ -38,13 +41,17 @@ public class BerylliumMeshUniformMaterial {
                 GL20.glUniform3f(sunDirectionLoc, tmp.x, tmp.y, tmp.z);
             }
             if (ambientWorldColorLoc != -1) {
-                Color ambientWorldColor = Sky.currentSky.currentAmbientColor;
+                Player player = InGame.getLocalPlayer();
+                Color ambientWorldColor = player.hasNightVision ? Color.GRAY : Color.BLACK;
                 GL20.glUniform3f(ambientWorldColorLoc, ambientWorldColor.r, ambientWorldColor.g, ambientWorldColor.b);
             }
             if (ambientSkyColorLoc != -1) {
                 Color ambientSkyColor = Sky.currentSky.currentSkyColor;
                 GL20.glUniform3f(ambientSkyColorLoc, ambientSkyColor.r, ambientSkyColor.g, ambientSkyColor.b);
             }
+            program.bindUniformFloat("u_fogDensity", Sky.currentSky.fogDensity);
+            program.bindUniform3f("u_playerLightColor", InGame.getUILightingTint());
+            program.bindUniformFloat("u_renderDistanceInChunks", GraphicsSettings.renderDistanceInChunks.getValue());
         } else {
             if (sunDirectionLoc != -1) {
                 GL20.glUniform3f(sunDirectionLoc, 0, 1, 0);
@@ -55,6 +62,8 @@ public class BerylliumMeshUniformMaterial {
             if (ambientSkyColorLoc != -1) {
                 GL20.glUniform3f(ambientSkyColorLoc, 1, 1, 1);
             }
+            program.bindUniformFloat("u_fogDensity", 0);
+            program.bindUniform3f("u_playerLightColor", Color.BLACK);
         }
         if (camPosLoc != -1) GL20.glUniform3f(camPosLoc, camera.position.x, camera.position.y, camera.position.z);
     }
--- src/client/java/me/zombii/beryllium/client/rendering/world/chunk/ChunkMesh.java
+++ src/client/java/me/zombii/beryllium/client/rendering/world/chunk/ChunkMesh.java
@@ -1,22 +1,11 @@
 package me.zombii.beryllium.client.rendering.world.chunk;
 
-import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.math.Matrix4;
-import com.badlogic.gdx.math.Vector3;
-import finalforeach.cosmicreach.entities.player.Player;
-import finalforeach.cosmicreach.gamestates.InGame;
-import finalforeach.cosmicreach.settings.GraphicsSettings;
-import finalforeach.cosmicreach.world.Sky;
-import me.zombii.beryllium.client.rendering.opengl.shader.BerylliumShaderProgram;
 import me.zombii.beryllium.client.rendering.tessellation.BerylliumMesh;
 import me.zombii.beryllium.client.rendering.tessellation.Tessallator;
 
 public class ChunkMesh extends BerylliumMesh {
 
     private final LayeredChunkMesh parent;
-
-    private final Vector3 sunDirection = new Vector3();
 
     public ChunkMesh(
             LayeredChunkMesh parent
@@ -32,23 +21,5 @@ public class ChunkMesh extends BerylliumMesh {
     @Override
     public void dump(Tessallator tessallator, boolean resetPos) {
         super.dump(tessallator, resetPos);
-    }
-
-    @Override
-    public void render(Camera camera, BerylliumShaderProgram program, Matrix4 modelMatrix) {
-        Sky sky = Sky.currentSky;
-
-        sky.getSunDirection(sunDirection);
-        program.bindUniform3f("u_sunDirection", sunDirection);
-        program.bindUniform3f("u_ambientSkyColor", sky.currentAmbientColor);
-
-        Player player = InGame.getLocalPlayer();
-        program.bindUniform3f("u_ambientWorldColor", player.hasNightVision ? Color.GRAY : Color.BLACK);
-        program.bindUniformFloat("u_fogDensity", sky.fogDensity);
-
-        program.bindUniformFloat("u_renderDistanceInChunks", GraphicsSettings.renderDistanceInChunks.getValue());
-        program.bindUniform3f("u_playerLightColor", InGame.getUILightingTint());
-
-        super.render(camera, program, modelMatrix);
     }
 }
